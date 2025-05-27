29435da9f6a6fc611d35d645fa740bd34f979622
LaurieByrne <byrnel68@tcd.ie>
2025-05-27 17:48:57 +0100

    Initial Commit

---
+++ MacKeyWizard/MacKeyWizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacKeyWizardApp.swift
+//  MacKeyWizard
+//
+//  Created by laurie byrne on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MacKeyWizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
